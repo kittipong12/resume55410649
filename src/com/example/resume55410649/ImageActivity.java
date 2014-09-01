f578164012e5c7ee51a0f071d95bfd8ad5fadf0f
kittipong12 <mel_kittipong@outlook.com>
2014-09-01 17:21:29 +0700

    Resume First

---
+++ src/com/example/resume55410649/ImageActivity.java
@@ -0,0 +1,14 @@
+package com.example.resume55410649;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ImageActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.image_kittipong);
+	}
+}
